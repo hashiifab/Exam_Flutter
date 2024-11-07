f01948cbc399853149463de8f7e10fb1a5a7bc25
hashiifab <hashiifidnsolo@gmail.com>
2024-11-07 13:57:58 +0700

    first comit

--- lib/lsit.dart
+++ lib/lsit.dart
@@ -6,6 +6,7 @@ class ListNote extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
           'List Note',
